340b98090d10c8d29648179b3ab3d1b18a5d3e99
D. Fuel <108496936+DeathFuel@users.noreply.github.com>
2024-10-02 14:12:55 +0200

    p1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
-// include the necessary libraries
+#include <string>
+
+using namespace std;
 
 class Car {
-    //attributes
-
-    // Constructor
-
-    // Method to display car information
-   
+	public:
+		Car(string _make, string _model, int _year) {
+			this->make = _make;
+			this->model = _model;
+			this->year = _year;
+		}
+		void displayInfo() {
+			printf("This car is a %s %s, produced %d.",
+					this->make.c_str(), this->model.c_str(), this->year
+				  );
+		}
+	private:
+		string make;
+		string model;
+		int year;
 };
 
 int main() {
-    // Create a Car object
-    
-    // Display car details
-    
+	Car c = Car("Fiat", "126p", 1978);
+
+	c.displayInfo();
+
     return 0;
 }
